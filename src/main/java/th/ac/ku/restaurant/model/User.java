c33d3f98a2e72ad05ce852268c533060a2e69b50
Pannawit <pannawit.p@ku.th>
2022-09-12 23:09:00 +0700

    first

--- src/main/java/th/ac/ku/restaurant/model/User.java
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -1,8 +1,14 @@
 package th.ac.ku.restaurant.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
+
+@Data
+@NoArgsConstructor
 @Entity
 public class User {
     @Id
@@ -11,48 +17,8 @@ public class User {
     private String username;
     private String password;
     private String firstName;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
     private String lastName;
+    private String email;
     private String role;
 // ให้ Generate..
 // - Getters และ Setters ทั้งหมด
